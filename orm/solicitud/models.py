2904507d7e3c692b3f128181edf4b29c1b765612
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-04-22 23:28:51 -0500

    Se creo el modelo de las app solicitudes y se realizo las migraciones correspondientes

--- orm/solicitud/models.py
+++ orm/solicitud/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Solicitud(models.Model):
+    TIPO_SOLICITUD = [
+        ('ACA','ACADEMICA'),
+        ('ADA','ADMINISTRATIVA'),
+        ('TEC','TECNICA'),
+        ('OTR','OTRA'),
+    ]
+
+    nombre_solicitud= models.CharField(max_length=100)
+    documento_identidad= models.CharField(max_length=100)
+    correo_electronico= models.EmailField()
+    telefono_contacto= models.CharField(max_length=100)
+    tipo_solicitud = models.CharField(max_length=3,choices=TIPO_SOLICITUD,default='ACA')
+
+    def __str__(self):
+        return f"{self.nombre_solicitud} - {self.documento_identidad} - {self.correo_electronico} - {self.telefono_contacto} - {self.tipo_solicitud}"
+
+
